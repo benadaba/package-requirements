90644497fb761a813fd3c1b82eb501572bd366c2
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2024-05-24 23:32:47 +0100

    Update Jenkins-redhat8-installation.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
@@ -2,8 +2,6 @@
 # Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux OS
 # You can execute this script as user-data when launching your EC2 VM.
 #!/bin/bash
-# Author: Prof Legah
-# date: 25/08/2020
 # Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux OS
 # You can execute this script as user-data when launching your EC2 VM.
 sudo timedatectl set-timezone America/New_York

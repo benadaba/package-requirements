87e71727f5a1f4f76e828425000a34d6d904b93e
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2025-06-30 21:33:28 +0100

    Create continuous-deployment.sh

---
+++ Tomcat-installation/continuous-deployment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Config
+TARGET_DIR="/home/ec2-user/maven-web-application/target"
+REMOTE_USER="ec2-user"
+REMOTE_HOST="172.31.12.12"
+REMOTE_PATH="/home/ec2-user/tomcatinfo"
+SSH_KEY="/path/to/private-key.pem"
+
+# Start checking loop
+while true; do
+    WAR_FILE=$(find "$TARGET_DIR" -maxdepth 1 -type f -name "*.war" | head -n 1)
+
+    if [ -f "$WAR_FILE" ]; then
+        echo "$(date): Found WAR file - $WAR_FILE"
+
+        # Attempt deployment using specified SSH key
+        #if scp -i "$SSH_KEY" "$WAR_FILE" "$REMOTE_USER@$REMO/home/ec2-user/tomcatinfo
+	if scp "$WAR_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"; then
+            echo "Successfully deployed"
+            exit 0
+        else
+            echo "$(date): Deployment failed. Retrying..."
+        fi
+    else
+        echo "$(date): No WAR file found in $TARGET_DIR"
+    fi
+
+    # Wait 60 seconds before checking again
+    #sleep 5
+done
